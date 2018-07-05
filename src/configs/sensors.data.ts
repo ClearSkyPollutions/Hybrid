b5da3e01115897fe7a556c5812fbd5c190e2caa0
Lerari <sihem.lerari@sogeti.com>
2018-07-05 15:35:36 +0200

    merge SH7 in dev

---
+++ src/configs/sensors.data.ts
@@ -0,0 +1,29 @@
+import { Sensor } from '../models/sensor.interface';
+
+export const SENSORS : Sensor[] = [
+    {
+        name : 'PMS5003',
+        desc : '',
+        image : 'assets/imgs/sensors/pms5003.png'
+    },
+    {
+        name : 'MQ2',
+        desc : '',
+        image : 'assets/imgs/sensors/mq2.bmp'
+    },
+    {
+        name : 'DHT22',
+        desc : '',
+        image : 'assets/imgs/sensors/dht22.jpg'
+    },
+    {
+        name : 'MISC',
+        desc : '',
+        image : 'assets/imgs/sensors/misc.png'
+    },
+    {
+        name : 'Microphone',
+        desc : '',
+        image : 'assets/imgs/sensors/microphone.jpg'
+    }
+];
---
+++ src/models/sensor.interface.ts
@@ -0,0 +1,5 @@
+export interface Sensor {
+    name: string;
+    desc: string;
+    image: string;
+}
